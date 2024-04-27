18c46e82559b0080b34fa8ce8926a16e4d85116d
vyunner <vyunner@gmail.com>
2024-04-27 09:55:19 +0500

    M  app/Http/Controllers/Product/ProductDestroyController.php

--- app/Http/Controllers/Product/ProductDestroyController.php
+++ app/Http/Controllers/Product/ProductDestroyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -22,14 +23,21 @@ class ProductDestroyController extends Controller
     {
         $product = Product::findOrFail($id);
 
-        if ($product->photo_url) {
-            $oldPath = 'public' . str_replace('/storage', '', $product->photo_url);
-            if (Storage::exists($oldPath)) {
-                Storage::delete($oldPath);
-            }
-        }
+        try {
+            $product->delete();
 
-        $product->delete();
+            if ($product->photo_url) {
+                $oldPath = 'public' . str_replace('/storage', '', $product->photo_url);
+                if (Storage::exists($oldPath)) {
+                    Storage::delete($oldPath);
+                }
+            }
+        } catch (QueryException $e) {
+            if ($e->getCode() === '23000') {
+                return $this->response(['message' => 'Невозможно удалить продукт, так как он используется в других записях.'], 409);
+            }
+            return $this->response(['message' => 'Произошла ошибка при удалении продукта.'], 500);
+        }
 
         return $this->response([], 'Продукт успешно удален!');
     }
