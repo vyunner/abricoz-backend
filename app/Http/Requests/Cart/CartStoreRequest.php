fea2a181a66a3ab5e6733ba5a217dfc284914099
vyunner <vyunner@gmail.com>
2024-04-28 15:36:16 +0500

    M  app/Http/Requests/Cart/CartStoreRequest.php

--- app/Http/Requests/Cart/CartStoreRequest.php
+++ app/Http/Requests/Cart/CartStoreRequest.php
@@ -23,7 +23,7 @@ class CartStoreRequest extends FormRequest
     {
         return [
             'product_id' => 'required|exists:products,id',
-            'quantity' => 'nullable|int'
+            'product_quantity' => 'nullable|int'
         ];
     }
 }
