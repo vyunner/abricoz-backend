69b083167ad98f88f15f5d1598711a23f8129986
vyunner <vyunner@gmail.com>
2024-04-28 15:39:43 +0500

    M  app/Http/Requests/Cart/CartStoreRequest.php M  app/Http/Requests/FavoriteProduct/FavoriteProductStoreRequest.php

--- app/Http/Requests/Cart/CartStoreRequest.php
+++ app/Http/Requests/Cart/CartStoreRequest.php
@@ -22,7 +22,7 @@ class CartStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'product_id' => 'required|exists:products,id',
+            'product_id' => 'required|int|exists:products,id',
             'product_quantity' => 'nullable|int'
         ];
     }
