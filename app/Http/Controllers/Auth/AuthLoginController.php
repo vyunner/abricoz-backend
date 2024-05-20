aa35eb9003be8235892a5bb9148db274065be822
vyunner <vyunner@gmail.com>
2024-05-20 16:08:04 +0500

    M  app/Http/Controllers/Auth/AuthLoginController.php

--- app/Http/Controllers/Auth/AuthLoginController.php
+++ app/Http/Controllers/Auth/AuthLoginController.php
@@ -23,6 +23,7 @@ class AuthLoginController extends Controller
         $data = $request->validated();
 
         $user = User::where(['phone' => $data['phone']])->first();
+        $user['roles'] = $user->getRoleNames();
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return $this->response(['user' => $user, 'token' => $token], 'Вы успешно вошли в аккаунт!');
