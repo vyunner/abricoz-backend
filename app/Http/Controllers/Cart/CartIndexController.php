cdcb3546873a8a2d2887d179b39d1d56fed00322
root <root@78315.vm>
2024-05-29 16:41:38 +0500

    1

--- app/Http/Controllers/Cart/CartIndexController.php
+++ app/Http/Controllers/Cart/CartIndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Cart;
 
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 /**
@@ -22,6 +23,29 @@ class CartIndexController extends Controller
 
         $carts = Cart::where(['user_id' => $user_id])->with('product')->get();
 
-        return $this->response($carts, 'Список корзины успешно загружен!');
+        $similarProducts = collect(); // Using a collection to manage products
+
+        if (!$carts->isEmpty()) {
+            foreach ($carts as $cart) {
+                if ($similarProducts->count() >= 15) {
+                    break;
+                }
+
+                $remaining = 15 - $similarProducts->count();
+
+                $similar = Product::where('subcategory_id', $cart->product->subcategory_id)
+                    ->where('id', '!=', $cart->product_id)
+                    ->whereNotIn('id', $similarProducts->pluck('id'))
+                    ->with(['subcategory', 'brand', 'country'])
+                    ->take($remaining)
+                    ->get();
+
+                $similarProducts = $similarProducts->merge($similar);
+            }
+        }
+
+        $similarProducts = $similarProducts->take(15);
+
+        return $this->response(['carts' => $carts, 'similarProducts' => $similarProducts], 'Список корзины успешно загружен!');
     }
 }
