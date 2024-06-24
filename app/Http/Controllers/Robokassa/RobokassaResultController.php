c039d1ba332a2a6312c1d83b3577a115d5fe9ad2
Vyacheslav <vyunner@gmail.com>
2024-06-24 18:21:31 +0500

    M  app/Http/Controllers/Robokassa/RobokassaResultController.php A  app/Models/RobokassaJson.php A  database/migrations/2024_04_27_053610_create_robokassa_json_table.php

--- app/Http/Controllers/Robokassa/RobokassaResultController.php
+++ app/Http/Controllers/Robokassa/RobokassaResultController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Robokassa;
 
 use App\Http\Controllers\Controller;
+use App\Models\RobokassaJson;
 use Illuminate\Http\Request;
 
 
@@ -18,7 +19,8 @@ class RobokassaResultController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return $request;
-        return $this->response($request, 'Заказ успешно создан!');
+        RobokassaJson::create(['data' => $request->all()]);
+
+        return $this->response([], 'Заказ успешно создан!');
     }
 }
