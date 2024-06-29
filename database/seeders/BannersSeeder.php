934d1ab79f2226e11189cfd37ccb55b6749f5c1f
Vyacheslav <vyunner@gmail.com>
2024-06-29 18:47:25 +0500

    M  database/seeders/BannersSeeder.php

--- database/seeders/BannersSeeder.php
+++ database/seeders/BannersSeeder.php
@@ -13,10 +13,9 @@ class BannersSeeder extends Seeder
      */
     public function run(): void
     {
-        Banner::create(['image_url' => '/storage/banners/1.png', 'number' => '1']);
-        Banner::create(['image_url' => '/storage/banners/2.png', 'number' => '2']);
-        Banner::create(['image_url' => '/storage/banners/3.png', 'number' => '3']);
-        Banner::create(['image_url' => '/storage/banners/4.png', 'number' => '4']);
-        Banner::create(['image_url' => '/storage/banners/5.png', 'number' => '5']);
+        Banner::create(['image_url' => '/storage/banners/2.png', 'number' => '1']);
+        Banner::create(['image_url' => '/storage/banners/3.png', 'number' => '2']);
+        Banner::create(['image_url' => '/storage/banners/4.png', 'number' => '3']);
+        Banner::create(['image_url' => '/storage/banners/5.png', 'number' => '4']);
     }
 }
