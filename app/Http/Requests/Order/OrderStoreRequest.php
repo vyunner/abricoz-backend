2821c02d3845f97ddf9a44dc57bdbef05bc1d623
Vyacheslav <vyunner@gmail.com>
2024-06-30 16:42:59 +0500

    M  app/Http/Requests/Order/OrderStoreRequest.php

--- app/Http/Requests/Order/OrderStoreRequest.php
+++ app/Http/Requests/Order/OrderStoreRequest.php
@@ -23,6 +23,8 @@ class OrderStoreRequest extends FormRequest
     {
         return [
             'delivery_interval_id' => 'required|int|exists:delivery_intervals,id',
+            'city_id' => 'required|int|exists:cities,id',
+            'district_id' => 'required|int|exists:districts,id',
             'address' => 'required|string',
             'address_comment' => 'nullable|string',
             'order_comment' => 'nullable|string',
