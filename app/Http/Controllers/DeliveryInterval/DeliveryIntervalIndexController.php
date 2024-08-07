3b3164ae32c2f174e73272c054a3d21a8955c01c
Vyacheslav <vyunner@gmail.com>
2024-08-07 12:39:57 +0500

    M  app/Http/Controllers/DeliveryInterval/DeliveryIntervalIndexController.php M  database/seeders/DeliveryIntervalsSeeder.php

--- app/Http/Controllers/DeliveryInterval/DeliveryIntervalIndexController.php
+++ app/Http/Controllers/DeliveryInterval/DeliveryIntervalIndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\DeliveryInterval;
 use App\Http\Controllers\Controller;
 use App\Models\DeliveryInterval;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 /**
  * @group DeliveryInterval
@@ -18,6 +19,29 @@ class DeliveryIntervalIndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return $this->response(DeliveryInterval::all(), 'Список временных интервалов успешно загружен!');
+        $currentTime = Carbon::now()->format('H:i');
+        $intervals = DeliveryInterval::all();
+
+        foreach ($intervals as $interval) {
+            if ($this->isCurrentTimeWithinInterval($currentTime, $interval->name)) {
+                $interval->is_active = 0;
+                $interval->save();
+            }
+        }
+
+        $activeIntervals = DeliveryInterval::where('is_active', 1)->get();
+
+        return response()->json([
+            'message' => 'Список временных интервалов успешно загружен!',
+            'data' => $activeIntervals,
+            'current_time' => Carbon::now()->toDateTimeString()
+        ]);
+    }
+
+    private function isCurrentTimeWithinInterval($currentTime, $intervalName)
+    {
+        list($start, $end) = explode('-', $intervalName);
+
+        return $currentTime >= $start && $currentTime <= $end;
     }
 }
