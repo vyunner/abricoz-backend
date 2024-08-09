e0c7848fc5e0ca889cca6cdc5d045d1890ded153
Vyacheslav <vyunner@gmail.com>
2024-08-09 17:16:23 +0500

    M  database/migrations/2024_04_12_144456_create_desktop_banners_table.php M  database/seeders/BannersSeeder.php A  database/seeders/mobile-banners/mandarin_en.jpeg D  database/seeders/mobile-banners/mandarin_en.png A  database/seeders/mobile-banners/mandarin_kz.jpeg D  database/seeders/mobile-banners/mandarin_kz.png A  database/seeders/mobile-banners/mandarin_ru.jpeg D  database/seeders/mobile-banners/mandarin_ru.png A  database/seeders/mobile-banners/strawberry_en.jpeg D  database/seeders/mobile-banners/strawberry_en.png A  database/seeders/mobile-banners/strawberry_kz.jpeg D  database/seeders/mobile-banners/strawberry_kz.png A  database/seeders/mobile-banners/strawberry_ru.jpeg D  database/seeders/mobile-banners/strawberry_ru.png

--- database/migrations/2024_04_12_144456_create_desktop_banners_table.php
+++ database/migrations/2024_04_12_144456_create_desktop_banners_table.php
@@ -15,6 +15,9 @@ return new class extends Migration {
             $table->string('image_url_ru');
             $table->string('image_url_kz');
             $table->string('image_url_en');
+            $table->string('title_ru')->nullable();
+            $table->string('title_kz')->nullable();
+            $table->string('title_en')->nullable();
             $table->integer('number')->default(1);
             $table->timestamps();
         });
