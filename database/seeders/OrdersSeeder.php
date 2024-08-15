1140d66e18ad282b88e9b53c9cf15132c0682aee
Vyacheslav <vyunner@gmail.com>
2024-08-15 12:42:22 +0500

    M  database/seeders/OrdersSeeder.php

--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -16,92 +16,109 @@ class OrdersSeeder extends Seeder
      */
     public function run(): void
     {
-        Order::create([
-            'user_id' => 1,
-            'order_status_id' => 1,
-            'delivery_interval_id' => 1,
-            'payment_type_id' => 1,
-            'city_id' => 1,
-            'district_id' => 1,
-            'address' => 'ул. Гоголя, 15, Алматы, Казахстан',
-            'address_comment' => 'Код от домофона 2580, второй подъезд, на третьем этаже.',
-            'order_comment' => 'Пожалуйста, выберите продукты без глютена. Спасибо!',
-            'delivery_date' => Carbon::now(),
-        ]);
-        Order::create([
-            'user_id' => 1,
-            'order_status_id' => 2,
-            'delivery_interval_id' => 2,
-            'payment_type_id' => 1,
-            'city_id' => 1,
-            'district_id' => 1,
-            'address' => 'проспект Назарбаева, 88, Алматы, Казахстан',
-            'address_comment' => 'Дом за магазином “Жибек Жолы”, парковка сзади. Позвоните, когда приедете.',
-            'order_comment' => 'Не заменяйте товары без согласования. Если нет яблок Голден, оставьте позицию пустой.',
-            'delivery_date' => Carbon::now(),
-        ]);
-        Order::create([
-            'user_id' => 1,
-            'order_status_id' => 3,
-            'delivery_interval_id' => 3,
-            'payment_type_id' => 1,
-            'city_id' => 1,
-            'district_id' => 1,
-            'address' => 'ул. Толе би, 59, Алматы, Казахстан',
-            'address_comment' => 'Белый забор, третий дом от угла. Собака во дворе не кусается.',
-            'order_comment' => 'Доставить до 18:00, пожалуйста. Очень важно!',
-            'delivery_date' => Carbon::now(),
-        ]);
-        Order::create([
-            'user_id' => 1,
-            'order_status_id' => 4,
-            'delivery_interval_id' => 4,
-            'payment_type_id' => 1,
-            'city_id' => 1,
-            'district_id' => 1,
-            'address' => 'пер. Макатаева, 123, Алматы, Казахстан',
-            'address_comment' => 'Подъезд с кодовым замком, код 3344. Звоните на мобильный при прибытии.',
-            'order_comment' => 'Если найдете экзотические фрукты, добавьте пару штук к заказу. Сюрприз!',
-            'delivery_date' => Carbon::now(),
-        ]);
-        Order::create([
-            'user_id' => 1,
-            'order_status_id' => 1,
-            'delivery_interval_id' => 3,
-            'payment_type_id' => 1,
-            'city_id' => 1,
-            'district_id' => 1,
-            'address' => 'пр. Сейфуллина, 501, Алматы, Казахстан',
-            'address_comment' => 'Офисный центр "Глобус", вход со стороны улицы. Охранник проводит.',
-            'order_comment' => 'Пожалуйста, упакуйте хрупкие продукты отдельно и пометьте соответствующим образом.',
-            'delivery_date' => Carbon::now(),
-        ]);
+        $ordersData = [
+            [
+                'user_id' => 1,
+                'order_status_id' => 1,
+                'delivery_interval_id' => 1,
+                'payment_type_id' => 1,
+                'city_id' => 1,
+                'district_id' => 1,
+                'address_street_and_house' => 'ул. Гоголя, 15',
+                'address_apartment' => '5',
+                'address_entrance' => '2',
+                'address_floor' => '3',
+                'address_comment' => 'Код от домофона 2580, второй подъезд, на третьем этаже.',
+                'delivery_date' => Carbon::now(),
+            ],
+            [
+                'user_id' => 1,
+                'order_status_id' => 2,
+                'delivery_interval_id' => 2,
+                'payment_type_id' => 1,
+                'city_id' => 1,
+                'district_id' => 1,
+                'address_street_and_house' => 'проспект Назарбаева, 88',
+                'address_apartment' => '12',
+                'address_entrance' => '1',
+                'address_floor' => '2',
+                'address_comment' => 'Дом за магазином “Жибек Жолы”, парковка сзади. Позвоните, когда приедете.',
+                'delivery_date' => Carbon::now(),
+            ],
+            [
+                'user_id' => 1,
+                'order_status_id' => 3,
+                'delivery_interval_id' => 3,
+                'payment_type_id' => 1,
+                'city_id' => 1,
+                'district_id' => 1,
+                'address_street_and_house' => 'ул. Толе би, 59',
+                'address_apartment' => '3',
+                'address_entrance' => '1',
+                'address_floor' => '1',
+                'address_comment' => 'Белый забор, третий дом от угла. Собака во дворе не кусается.',
+                'delivery_date' => Carbon::now(),
+            ],
+            [
+                'user_id' => 1,
+                'order_status_id' => 4,
+                'delivery_interval_id' => 4,
+                'payment_type_id' => 1,
+                'city_id' => 1,
+                'district_id' => 1,
+                'address_street_and_house' => 'пер. Макатаева, 123',
+                'address_apartment' => '14',
+                'address_entrance' => '3',
+                'address_floor' => '5',
+                'address_comment' => 'Подъезд с кодовым замком, код 3344. Звоните на мобильный при прибытии.',
+                'delivery_date' => Carbon::now(),
+            ],
+            [
+                'user_id' => 1,
+                'order_status_id' => 1,
+                'delivery_interval_id' => 3,
+                'payment_type_id' => 1,
+                'city_id' => 1,
+                'district_id' => 1,
+                'address_street_and_house' => 'пр. Сейфуллина, 501',
+                'address_apartment' => '22',
+                'address_entrance' => '4',
+                'address_floor' => '7',
+                'address_comment' => 'Офисный центр "Глобус", вход со стороны улицы. Охранник проводит.',
+                'delivery_date' => Carbon::now(),
+            ],
+        ];
 
-        $orders = Order::all();
+        foreach ($ordersData as $orderData) {
+            $order = Order::create($orderData);
 
-        foreach ($orders as $order) {
             $productsPrice = 0;
+
             for ($i = 0; $i < 5; $i++) {
                 $product_price = rand(1, 15);
                 $product_quantity = rand(1, 15);
+                $product_discount = rand(0, 1);
+                $priceWithDiscount = $product_price * ((100 - $product_discount) / 100);
 
                 DB::table('order_products')->insert([
                     'order_id' => $order->id,
                     'product_id' => rand(1, 5),
                     'product_quantity' => $product_quantity,
                     'product_price' => $product_price,
-                    'product_discount' => rand(0, 1),
+                    'product_discount' => $product_discount,
+                    'product_price_with_discount' => $priceWithDiscount,
                 ]);
 
-                $productsPrice += $product_price * $product_quantity;
-                $deliveryPrice = District::where(['id' => $order->district_id])->first()->delivery_price;
-
-                if ($i == 4) {
-                    $order->update(['products_price' => $productsPrice]);
-                    $order->update(['delivery_price' => $deliveryPrice]);
-                    $order->update(['total_price' => $productsPrice + $deliveryPrice]);
-                }
+                $productsPrice += $priceWithDiscount * $product_quantity;
             }
+
+            $deliveryPrice = District::find($order->district_id)->delivery_price;
+
+            $order->update([
+                'products_price' => $productsPrice,
+                'delivery_price' => $deliveryPrice,
+                'total_price' => $productsPrice + $deliveryPrice,
+            ]);
         }
     }
 }
