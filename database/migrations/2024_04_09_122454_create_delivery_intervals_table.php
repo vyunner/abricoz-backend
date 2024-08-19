881933b569f981eeb0bc295906304d7ee009a084
Vyacheslav <vyunner@gmail.com>
2024-08-19 15:47:51 +0500

    M  database/migrations/2024_04_09_122454_create_delivery_intervals_table.php

--- database/migrations/2024_04_09_122454_create_delivery_intervals_table.php
+++ database/migrations/2024_04_09_122454_create_delivery_intervals_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('delivery_intervals', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('is_active')->default(1);
             $table->timestamps();
         });
     }
