9328d33f5173922cef972e7f70711b3ee8d2c6d4
Vyacheslav <vyunner@gmail.com>
2024-09-23 13:19:47 +0500

    M  app/Http/Kernel.php M  config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,19 +15,19 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('FRONTEND_URLS')),
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => false,
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
