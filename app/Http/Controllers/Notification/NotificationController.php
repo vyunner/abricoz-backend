1242a3c2c0fed2377ef7d3e97ea42b369ee6ac6f
Vyacheslav <vyunner@gmail.com>
2024-09-26 18:03:08 +0500

    M  .env.example M  app/Http/Controllers/Notification/NotificationController.php M  app/Http/Controllers/Order/OrderStoreController.php A  app/Services/FirebaseNotificationService.php D  app/Services/NotificationService.php M  config/firebase.php M  database/seeders/OrderSatusesSeeder.php

--- app/Http/Controllers/Notification/NotificationController.php
+++ app/Http/Controllers/Notification/NotificationController.php
@@ -3,31 +3,39 @@
 namespace App\Http\Controllers\Notification;
 
 use App\Http\Controllers\Controller;
-use App\Services\NotificationService;
+use App\Services\FirebaseNotificationService;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    protected $notificationService;
+    protected $firebaseNotificationService;
 
-    public function __construct(NotificationService $notificationService)
+    public function __construct(FirebaseNotificationService $firebaseNotificationService)
     {
-        $this->notificationService = $notificationService;
+        $this->firebaseNotificationService = $firebaseNotificationService;
     }
 
     public function __invoke(Request $request)
     {
-        $fcmToken = $request->input('fcm_token');
-        $title = $request->input('title');
-        $body = $request->input('body');
-        $data = $request->input('data', []);
+        try {
+            $response = $this->firebaseNotificationService->sendNotification(
+                'app1', // Идентификатор приложения ('app1' или 'app2')
+                $request->input('fcm_token'), // Токен устройства получателя
+                [
+                    'title' => $request->input('title'),
+                    'body' => $request->input('body'),
+                    'data' => [
+                        'key1' => 'value1',
+                        'key2' => 'value2',
+                    ],
+                ]
+            );
 
-        $result = $this->notificationService->sendNotification($fcmToken, $title, $body, $data);
-
-        if ($result) {
-            return response()->json(['message' => 'Уведомление успешно отправлено']);
-        } else {
-            return response()->json(['message' => 'Ошибка при отправке уведомления'], 500);
+            // Обработка успешного ответа
+            dd($response);
+        } catch (\Exception $e) {
+            // Обработка ошибок
+            dd($e->getMessage());
         }
     }
 }
