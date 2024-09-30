9e974dad078935cdd1ecd3be22894cdd1327b985
Vyacheslav <vyunner@gmail.com>
2024-09-30 18:49:23 +0500

    M  app/Http/Controllers/Auth/AuthLoginController.php M  app/Http/Controllers/Warehouseman/WarehousemanAcceptController.php A  app/Http/Requests/Warehouseman/WarehousemanAcceptRequest.php A  app/Models/OrderAssignment.php A  database/migrations/2024_04_27_053610_create_order_assignments_table.php

--- app/Http/Controllers/Warehouseman/WarehousemanAcceptController.php
+++ app/Http/Controllers/Warehouseman/WarehousemanAcceptController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Warehouseman;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SubCategory\SubCategoryStoreRequest;
 use App\Http\Requests\UserDevice\UserDeviceStoreRequest;
+use App\Http\Requests\Warehouseman\WarehousemanAcceptRequest;
+use App\Models\OrderAssignment;
 use App\Models\SubCategory;
 use App\Models\UserDevice;
 use Illuminate\Http\Request;
@@ -20,10 +22,13 @@ class WarehousemanAcceptController extends Controller
      * @param SubCategoryStoreRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function __invoke(UserDeviceStoreRequest $request)
+    public function __invoke(WarehousemanAcceptRequest $request)
     {
         $validatedData = $request->validated();
+        $validatedData['role_id'] = 2;
+        $validatedData['user_id'] = $request->user()->id;
 
+        OrderAssignment::create($validatedData);
         return $this->response($userDevice, $message);
     }
 }
