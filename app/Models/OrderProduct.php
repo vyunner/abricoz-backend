a37da990eb34d0bd17b89786302286214eb6d77a
Vyacheslav <vyunner@gmail.com>
2024-10-01 17:23:33 +0500

    M  app/Models/Order.php M  app/Models/OrderAssignment.php M  app/Models/OrderProduct.php

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -18,6 +18,6 @@ class OrderProduct extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class, 'product_id');
     }
 }
