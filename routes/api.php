309c4daaca061391728db2b5aa78ad8449e2691d
Vyacheslav <vyunner@gmail.com>
2024-10-01 17:03:58 +0500

    M  routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -194,6 +194,7 @@ Route::group(['prefix' => '/warehouseman'], function () {
         Route::get('/show/{id}', \App\Http\Controllers\Warehouseman\WarehousemanShowController::class);
         Route::post('/accept', \App\Http\Controllers\Warehouseman\WarehousemanAcceptController::class);
         Route::post('/complete', \App\Http\Controllers\Warehouseman\WarehousemanCompleteController::class);
+        Route::post('/get-current-order', \App\Http\Controllers\Warehouseman\WarehousemanGetCurrentOrderController::class);
     });
 });
 
