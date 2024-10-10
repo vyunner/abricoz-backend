66829cb4c6e5683180ee343c4d3e0ba4d9b47761
Vyacheslav <vyunner@gmail.com>
2024-10-10 15:04:36 +0500

    M  app/Http/Controllers/Dispatcher/DispatcherUnassignOrdersController.php M  app/Http/Requests/Dispatcher/DispatcherUnassignOrderRequest.php

--- app/Http/Controllers/Dispatcher/DispatcherUnassignOrdersController.php
+++ app/Http/Controllers/Dispatcher/DispatcherUnassignOrdersController.php
@@ -11,18 +11,15 @@ class DispatcherUnassignOrdersController extends Controller
 {
     public function __invoke(DispatcherUnassignOrderRequest $request)
     {
-        // Валидируем запрос и получаем данные
         $data = $request->validated();
-        $orderUserIds = $data['order_user_ids'];
+
+        $userId = $data['user_id'];
+        $orderIds = $data['order_ids'];
 
         DB::beginTransaction();
 
         try {
-            foreach ($orderUserIds as $item) {
-                $userId = $item['user_id']; // Получаем user_id
-                $orderId = $item['order_id']; // Получаем order_id
-
-                // Ищем назначение
+            foreach ($orderIds as $orderId) {
                 $assignment = OrderAssignment::where('order_id', $orderId)
                     ->where('user_id', $userId)
                     ->where('role_id', 3) // Айди роли курьера
@@ -33,16 +30,13 @@ class DispatcherUnassignOrdersController extends Controller
                     return $this->response(null, "Назначение заказа ID $orderId на курьера ID $userId не найдено", 400);
                 }
 
-                // Удаляем назначение
                 $assignment->delete();
             }
 
-            // Фиксируем изменения в базе данных
             DB::commit();
 
             return $this->response(null, 'Заказы успешно сняты с курьера', 200);
         } catch (\Exception $e) {
-            // В случае ошибки откатываем транзакцию
             DB::rollBack();
             return $this->response(null, 'Произошла ошибка при снятии заказов с курьера', 500);
         }
