6a8a769d2deaf028daa27053baaaa1a8a9607aaa
Vyacheslav <vyunner@gmail.com>
2024-10-24 15:08:03 +0500

    M  app/Http/Controllers/Warehouse/WarehouseCreateProductController.php M  app/Http/Requests/Warehouse/WarehouseCreateProductRequest.php M  database/migrations/2024_04_09_122453_create_products_table.php

--- app/Http/Controllers/Warehouse/WarehouseCreateProductController.php
+++ app/Http/Controllers/Warehouse/WarehouseCreateProductController.php
@@ -13,10 +13,6 @@ class WarehouseCreateProductController extends Controller
     {
         $data = $request->validated();
 
-        if ($data['is_active'] == 1 && $data['amount'] <= 0) {
-            return $this->response(null, 'Cannot create an active product with zero or negative amount', 400);
-        }
-
         if ($request->hasFile('image')) {
             $path = $request->file('image')->store('public/products');
             $data['photo_url'] = Storage::url($path);
