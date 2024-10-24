bd949fd8942454f6aaaf00ff6bb5c37cc86035c5
Vyacheslav <vyunner@gmail.com>
2024-10-24 14:48:52 +0500

    M  app/Http/Controllers/Warehouse/WarehouseCreateProductController.php M  app/Http/Requests/Warehouse/WarehouseCreateProductRequest.php

--- app/Http/Controllers/Warehouse/WarehouseCreateProductController.php
+++ app/Http/Controllers/Warehouse/WarehouseCreateProductController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Warehouse;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Warehouse\WarehouseProductRequest;
+use App\Http\Requests\Warehouse\WarehouseCreateProductRequest;
 use App\Models\Product;
 use Illuminate\Support\Facades\Storage;
 
 class WarehouseCreateProductController extends Controller
 {
-    public function __invoke(WarehouseProductRequest $request)
+    public function __invoke(WarehouseCreateProductRequest $request)
     {
         $data = $request->validated();
 
@@ -17,8 +17,8 @@ class WarehouseCreateProductController extends Controller
             return $this->response(null, 'Cannot create an active product with zero or negative amount', 400);
         }
 
-        if ($request->hasFile('photo_url')) {
-            $path = $request->file('photo_url')->store('public/products');
+        if ($request->hasFile('image')) {
+            $path = $request->file('image')->store('public/products');
             $data['photo_url'] = Storage::url($path);
         }
 
