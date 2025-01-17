916d186119f1452499dfc9c92d54f032f7add49f
kosynka <sayat.kaldarbekov.00@gmail.com>
2025-01-18 00:04:55 +0500

    AB-38

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function response($data, $message, $code = 200)
+    public function response(mixed $data, string $message, int $code = 200): JsonResponse
     {
-        $response = [];
-        $response['data'] = ($code == 200) ? $data : null;
-        $response['message'] = $message;
-        $response['http_code'] = $code;
-        $response['status'] = ($code == 200) ? 'success' : 'failed';
+        $response = [
+            'data' => ($code == Response::HTTP_OK) ? $data : null,
+            'message' => $message,
+            'http_code' => $code,
+            'status' => ($code === Response::HTTP_OK) ? 'success' : 'failed',
+        ];
 
         return response()->json($response, $code);
     }
--- app/Interfaces/MobizonServiceInterface.php
+++ app/Interfaces/MobizonServiceInterface.php
@@ -4,9 +4,9 @@ namespace App\Interfaces;
 
 interface MobizonServiceInterface
 {
-    public function sendSmsMessage($recipient, $text, $from = null, $params = []): array;
+    public function sendSmsMessage(string $recipient, string $text, mixed $from = null, array $params = []): array;
 
-    public function getSmsStatus($ids): array;
+    public function getSmsStatus(array|string $ids): array;
 
-    public function listMessages($criteria = [], $pagination = [], $sort = [], $withNumberInfo = 0): array;
+    public function listMessages(array $criteria = [], array $pagination = [], array $sort = [], int $withNumberInfo = 0): array;
 }
