aab8be70216bb378872a109958c3beda617f9066
Vyacheslav <vyunner@gmail.com>
2025-01-18 19:00:54 +0500

    M  app/Models/OrderStatus.php

--- app/Models/OrderStatus.php
+++ app/Models/OrderStatus.php
@@ -9,12 +9,12 @@ class OrderStatus extends Model
 {
     use HasFactory;
 
-    public static const IN_PROCESS = 1;
-    public static const ASSEMBLING = 2;
-    public static const WAITING_FOR_COURIER = 3;
-    public static const ON_THE_WAY = 4;
-    public static const DELIVERED = 5;
-    public static const CANCELLED = 6;
+    public const IN_PROCESS = 1;
+    public const ASSEMBLING = 2;
+    public const WAITING_FOR_COURIER = 3;
+    public const ON_THE_WAY = 4;
+    public const DELIVERED = 5;
+    public const CANCELLED = 6;
 
     protected $guarded = [];
 }
