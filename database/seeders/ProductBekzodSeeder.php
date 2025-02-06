f5960e2726509da0078dbdfbdb0af93e32627f1c
Vyacheslav <vyunner@gmail.com>
2025-02-06 18:04:19 +0500

    M  database/seeders/ProductBekzodSeeder.php A  "database/seeders/xlsx/dried fruits.xlsx"

--- database/seeders/ProductBekzodSeeder.php
+++ database/seeders/ProductBekzodSeeder.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-class ProductXlsxSeeder extends Seeder
+class ProductBekzodSeeder extends Seeder
 {
     public function run()
     {
         // Путь к файлу Excel
-        $filePath = base_path('database/seeders/xlsx/2.xlsx');
+        $filePath = base_path('database/seeders/xlsx/dried fruits.xlsx');
 
         // Загружаем файл Excel
         try {
@@ -37,6 +37,7 @@ class ProductXlsxSeeder extends Seeder
             // Добавляем в базу данных
             DB::table('products')->insert([
                 'subcategory_id' => $productData['subcategory_id'],
+                'where' => $productData['where'],
                 'manufacturer' => $productData['manufacturer'],
                 'name_ru' => $productData['name_ru'],
                 'name_kz' => $productData['name_kz'],
@@ -44,63 +45,15 @@ class ProductXlsxSeeder extends Seeder
                 'description_kz' => $productData['description_kz'],
                 'price' => $productData['price'],
                 'discount' => $productData['discount'],
-                'price_with_discount' => $productData['price'] - ($productData['price'] * $productData['discount'] / 100),
-                'photo_url' => $productData['photo_url'],
+                'price_with_discount' => $productData['price_with_discount'],
+                'photo_url' => 'null',
                 'weight' => $productData['weight'],
                 'calories' => $productData['calories'],
                 'proteins' => $productData['proteins'],
                 'fats' => $productData['fats'],
                 'carbohydrates' => $productData['carbohydrates'],
                 'is_active' => $productData['is_active'],
-                'amount' => 1000,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
-
-        // Путь к файлу Excel
-        $filePath = base_path('database/seeders/xlsx/papapa.xlsx');
-
-        // Загружаем файл Excel
-        try {
-            $spreadsheet = IOFactory::load($filePath);
-        } catch (\Exception $e) {
-            echo 'Ошибка загрузки файла: ', $e->getMessage();
-            return;
-        }
-
-        // Получаем активный лист
-        $worksheet = $spreadsheet->getActiveSheet();
-
-        // Преобразуем лист в массив
-        $rows = $worksheet->toArray();
-
-        // Первая строка - заголовки
-        $headers = array_shift($rows);
-
-        foreach ($rows as $row) {
-            // Сопоставляем заголовки с данными
-            $productData = array_combine($headers, $row);
-
-            // Добавляем в базу данных
-            DB::table('products')->insert([
-                'subcategory_id' => $productData['subcategory_id'],
-                'manufacturer' => $productData['manufacturer'],
-                'name_ru' => $productData['name_ru'],
-                'name_kz' => $productData['name_kz'],
-                'description_ru' => $productData['description_ru'],
-                'description_kz' => $productData['description_kz'],
-                'price' => $productData['price'],
-                'discount' => $productData['discount'],
-                'price_with_discount' => $productData['price'] - ($productData['price'] * $productData['discount'] / 100),
-                'photo_url' => $productData['photo_url'],
-                'weight' => $productData['weight'],
-                'calories' => $productData['calories'],
-                'proteins' => $productData['proteins'],
-                'fats' => $productData['fats'],
-                'carbohydrates' => $productData['carbohydrates'],
-                'is_active' => $productData['is_active'],
-                'amount' => 1000,
+                'amount' => $productData['amount'],
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
