ed724d8609589a05adafbe65a6c52bb6a73721b6
kosynka <sayat.kaldarbekov.00@gmail.com>
2025-02-08 00:06:49 +0500

    log viewer fixed

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '3fb5b943f9797d60f9b59fbab97af22c9401/*',
     ];
 }
