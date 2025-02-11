483e8ec24c1d6b06d7b14b4289012dad9ab1b8e2
Vyacheslav <vyunner@gmail.com>
2025-02-11 15:30:32 +0500

    M  app/Providers/AppServiceProvider.php M  config/app.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,8 +11,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-        $this->app->register(TelescopeServiceProvider::class);
     }
 
     /**
