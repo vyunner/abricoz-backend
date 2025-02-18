7eb66cdb919f97e213fc9d0e37b3e960004cf62f
Vyacheslav <vyunner@gmail.com>
2025-02-18 17:04:05 +0500

    M  app/Http/Controllers/DeliveryInterval/DeliveryIntervalIndexController.php

--- app/Http/Controllers/DeliveryInterval/DeliveryIntervalIndexController.php
+++ app/Http/Controllers/DeliveryInterval/DeliveryIntervalIndexController.php
@@ -20,14 +20,13 @@ class DeliveryIntervalIndexController extends Controller
     {
         $available_intervals = [];
         $current_time = now();
-        return $current_time;
         $intervals = DeliveryInterval::all();
 
         $dates = [
             today()->addDays(1),
             today()->addDays(2),
             // Если текущее время больше 19:00, то вместо сегодняшнего дня дается на выбор после-после-завтра
-            $current_time->copy()->format('H') > 19 ? today()->addDays(3) : today(),
+//            $current_time->copy()->format('H') > 19 ? today()->addDays(3) : today(),
         ];
 
         // Преобразование временных интервалов в отформатированный массив
