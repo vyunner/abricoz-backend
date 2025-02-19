73d98223e856a9c0856e2b593ed1dee66d36dc56
kosynka <sayat.kaldarbekov.00@gmail.com>
2025-02-19 22:58:25 +0500

    removed statefull error

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -86,7 +86,7 @@ return [
     */
 
     'api_middleware' => [
-        \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        // \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
