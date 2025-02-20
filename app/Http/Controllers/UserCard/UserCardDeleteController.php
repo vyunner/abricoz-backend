4bc6097a0851f21ca8d871aefb83f2de59a6f52d
Vyacheslav <vyunner@gmail.com>
2025-02-20 17:04:15 +0500

    M  app/Http/Controllers/UserCard/UserCardDeleteController.php

--- app/Http/Controllers/UserCard/UserCardDeleteController.php
+++ app/Http/Controllers/UserCard/UserCardDeleteController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\UserCard;
 
 use App\Http\Controllers\Controller;
 use App\Models\UserCard;
+use App\Services\EpayService;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -12,6 +14,13 @@ use Illuminate\Http\Request;
  */
 class UserCardDeleteController extends Controller
 {
+    protected EpayService $epayService;
+
+    public function __construct(EpayService $epayService)
+    {
+        $this->epayService = $epayService;
+    }
+
     /**
      * Удаление карты юзера
      * @param Request $request
@@ -28,6 +37,22 @@ class UserCardDeleteController extends Controller
         if (!$userCard) {
             return $this->response(null, 'Карта не найдена или не принадлежит вам.', 404);
         }
+
+        $config = config('epay');
+
+        $token = $this->epayService->getToken([
+            'grant_type' => 'client_credentials',
+            'scope' => 'webapi usermanagement email_send verification statement statistics payment',
+            'client_id' => $config['client_id'],
+            'client_secret' => $config['client_secret'],
+        ]);
+
+        $url = "https://epay-api.homebank.kz/card/deactivate/{$userCard->cardID}";
+        $response = Http::withHeaders([
+            'Authorization' => "Bearer {$token['access_token']}",
+        ])->post($url);
+
+        return $response;
 
         // Удаляем карту
         $userCard->delete();
