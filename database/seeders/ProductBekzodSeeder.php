443b839c2740e3a0367e08f515a3895a70d0a6bc
Vyacheslav <vyunner@gmail.com>
2025-02-24 00:12:38 +0500

    M  database/seeders/ProductBekzodSeeder.php A  database/seeders/xlsx/2402.xlsx

--- database/seeders/ProductBekzodSeeder.php
+++ database/seeders/ProductBekzodSeeder.php
@@ -11,7 +11,7 @@ class ProductBekzodSeeder extends Seeder
     public function run()
     {
         // Путь к файлу Excel
-        $filePath = base_path('database/seeders/xlsx/dried fruits (2).xlsx');
+        $filePath = base_path('database/seeders/xlsx/2402.xlsx');
 
         // Загружаем файл Excel
         try {
