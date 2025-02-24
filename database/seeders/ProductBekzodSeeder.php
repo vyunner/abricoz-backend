8cc933ce3b62dccf2ccdd196b5fe073e576b6662
Vyacheslav <vyunner@gmail.com>
2025-02-25 01:16:27 +0500

    M  database/seeders/ProductBekzodSeeder.php A  database/seeders/xlsx/dastan24.02.xlsx

--- database/seeders/ProductBekzodSeeder.php
+++ database/seeders/ProductBekzodSeeder.php
@@ -11,7 +11,7 @@ class ProductBekzodSeeder extends Seeder
     public function run()
     {
         // Путь к файлу Excel
-        $filePath = base_path('database/seeders/xlsx/2402.xlsx');
+        $filePath = base_path('database/seeders/xlsx/dastan24.02.xlsx');
 
         // Загружаем файл Excel
         try {
