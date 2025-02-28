8f038c7ea5e5347abc90c83ab6b05eb8d54ad27f
Vyacheslav <vyunner@gmail.com>
2025-02-28 16:02:16 +0500

    A  database/migrations/2025_01_31_150846_add_subcategory_id_to_categories.php

---
+++ database/migrations/2025_01_31_150846_add_subcategory_id_to_categories.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->unsignedBigInteger('forward_subcategory_id')->nullable()->after('id');
+            $table->foreign('forward_subcategory_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeign(['forward_subcategory_id']);
+            $table->dropColumn('forward_subcategory_id');
+        });
+    }
+};
