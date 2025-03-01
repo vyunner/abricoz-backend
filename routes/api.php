14c2abec6fe66147b037d9e94bc249ffeb407d53
Vyacheslav <vyunner@gmail.com>
2025-03-02 00:55:04 +0500

    M  routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -110,11 +110,9 @@ Route::group(['prefix' => '/desktop-banner'], function () {
 });
 
 Route::group(['prefix' => '/mobile-banner'], function () {
-    Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () {
-        Route::post('/update', Controllers\MobileBanner\MobileBannerUpdateController::class);
-        Route::post('/store', Controllers\MobileBanner\MobileBannerStoreController::class);
-        Route::delete('/delete/{id}', Controllers\MobileBanner\MobileBannerDestroyController::class);
-    });
+    Route::post('/update', Controllers\MobileBanner\MobileBannerUpdateController::class);
+    Route::post('/store', Controllers\MobileBanner\MobileBannerStoreController::class);
+    Route::delete('/delete/{id}', Controllers\MobileBanner\MobileBannerDestroyController::class);
 
     Route::get('/index', Controllers\MobileBanner\MobileBannerIndexController::class);
 });
