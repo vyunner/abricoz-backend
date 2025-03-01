b936a22661502634f3e578c1eaf297856984c6c5
Vyacheslav <vyunner@gmail.com>
2025-03-01 23:04:23 +0500

    M  app/Http/Controllers/Category/CategoryShowController.php

--- app/Http/Controllers/Category/CategoryShowController.php
+++ app/Http/Controllers/Category/CategoryShowController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Category;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 
 /**
@@ -19,6 +20,16 @@ class CategoryShowController extends Controller
     public function __invoke(int $id)
     {
         $category = Category::with('subcategories')->findOrFail($id);
+
+        if ($id === 27) {
+            $subcategoriesWithDiscounts = Subcategory::select('subcategories.*')
+                ->join('products', 'products.subcategory_id', '=', 'subcategories.id')
+                ->whereNotNull('products.discount')
+                ->distinct()
+                ->get();
+
+            $category->setRelation('subcategories', $subcategoriesWithDiscounts);
+        }
 
         return $this->response($category, 'Категория и ее подкатегории успешно отображены!');
     }
