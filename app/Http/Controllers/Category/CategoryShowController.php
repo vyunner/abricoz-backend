98546744e14bbb814448fc798aa0250e175d9258
Vyacheslav <vyunner@gmail.com>
2025-03-01 23:08:58 +0500

    M  app/Http/Controllers/Category/CategoryShowController.php

--- app/Http/Controllers/Category/CategoryShowController.php
+++ app/Http/Controllers/Category/CategoryShowController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\SubCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @group Category
