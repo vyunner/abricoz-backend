9a3bf80181a1f8290e4c3ffc493e17369bba138d
Vyacheslav <vyunner@gmail.com>
2025-03-02 15:05:18 +0500

    M  app/Http/Requests/Category/CategoryStoreRequest.php M  app/Http/Requests/Category/CategoryUpdateRequest.php M  app/Http/Requests/SubCategory/SubCategoryStoreRequest.php M  app/Http/Requests/SubCategory/SubCategoryUpdateRequest.php

--- app/Http/Requests/SubCategory/SubCategoryUpdateRequest.php
+++ app/Http/Requests/SubCategory/SubCategoryUpdateRequest.php
@@ -27,7 +27,7 @@ class SubCategoryUpdateRequest extends FormRequest
             'name_ru' => 'nullable|string',
             'name_kz' => 'nullable|string',
             'is_active' => 'nullable|boolean',
-            'priority_number' => 'nullable',
+            'priority_number' => 'nullable|integer',
         ];
     }
 }
