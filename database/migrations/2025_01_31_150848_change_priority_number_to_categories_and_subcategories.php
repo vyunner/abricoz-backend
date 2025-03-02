0010f40bbbace37d6478f6eb1b51a2462029ec0e
Vyacheslav <vyunner@gmail.com>
2025-03-02 15:11:32 +0500

    M  app/Http/Requests/Category/CategoryStoreRequest.php M  app/Http/Requests/Category/CategoryUpdateRequest.php M  app/Http/Requests/SubCategory/SubCategoryStoreRequest.php M  app/Http/Requests/SubCategory/SubCategoryUpdateRequest.php A  database/migrations/2025_01_31_150848_change_priority_number_to_categories_and_subcategories.php

---
+++ database/migrations/2025_01_31_150848_change_priority_number_to_categories_and_subcategories.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->integer('priority_number')->default(0)->change();
+        });
+
+        Schema::table('subcategories', function (Blueprint $table) {
+            $table->integer('priority_number')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->integer('priority_number')->nullable()->change();
+        });
+
+        Schema::table('subcategories', function (Blueprint $table) {
+            $table->integer('priority_number')->nullable()->change();
+        });
+    }
+};
