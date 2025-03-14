318c30e07b896fca5b4f694f11b3793e4847b23e
Vyacheslav <vyunner@gmail.com>
2025-03-14 14:12:32 +0500

    R  app/Models/Reciept.php -> app/Models/Receipt.php

--- app/Models/Receipt.php
+++ app/Models/Receipt.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Order;
 
 class Receipt extends Model
 {
@@ -15,7 +17,7 @@ class Receipt extends Model
         'ticket_print_url'
     ];
 
-    public function order()
+    public function order(): BelongsTo
     {
         return $this->belongsTo(Order::class);
     }
