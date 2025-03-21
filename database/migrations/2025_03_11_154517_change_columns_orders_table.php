4f6183ddd0f1007c5aa15ea6d6e90b5902afd9b0
Vyacheslav <vyunner@gmail.com>
2025-03-21 13:42:42 +0500

    A  database/migrations/2025_03_11_154517_change_columns_orders_table.php

---
+++ database/migrations/2025_03_11_154517_change_columns_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->text('address_apartment')->nullable()->change();
+            $table->text('address_entrance')->nullable()->change();
+            $table->text('address_floor')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->text('address_apartment')->nullable(false)->change();
+            $table->text('address_entrance')->nullable(false)->change();
+            $table->text('address_floor')->nullable(false)->change();
+        });
+    }
+};
