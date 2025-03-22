6a65f12ab041504dd5434d39865e21401fb1fba6
Vyacheslav <vyunner@gmail.com>
2025-03-22 14:53:31 +0500

    A  app/Http/Controllers/App/AppGetSettingsController.php

---
+++ app/Http/Controllers/App/AppGetSettingsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\App;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+/**
+ * @group App
+ */
+class AppGetSettingsController extends Controller
+{
+    /**
+     * Status
+     * @param Request $request
+     * @return int[]
+     */
+    public function __invoke(Request $request)
+    {
+        return [
+            'isAppActive' => 1,            // Приложение активно
+            'minOrderAmount' => 4000,         // Минимальная сумма заказа
+            'isCashPaymentActive' => 0   // Разрешена ли оплата наличными
+        ];
+    }
+}
