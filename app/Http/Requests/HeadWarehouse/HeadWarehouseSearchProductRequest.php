12905c970603adff1e177ce32d26be0ea3f1bb40
Vyacheslav <vyunner@gmail.com>
2025-03-23 15:24:16 +0500

    M  app/Http/Requests/HeadWarehouse/HeadWarehouseSearchProductRequest.php

--- app/Http/Requests/HeadWarehouse/HeadWarehouseSearchProductRequest.php
+++ app/Http/Requests/HeadWarehouse/HeadWarehouseSearchProductRequest.php
@@ -22,7 +22,7 @@ class HeadWarehouseSearchProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'search' => 'string',
+            'search' => 'required|string',
         ];
     }
 }
