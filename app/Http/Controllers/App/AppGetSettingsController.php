dee564530ab5abd84bfbf682375195891dee3c54
Vyacheslav <vyunner@gmail.com>
2025-03-26 17:23:31 +0500

    M  app/Http/Controllers/App/AppGetSettingsController.php

--- app/Http/Controllers/App/AppGetSettingsController.php
+++ app/Http/Controllers/App/AppGetSettingsController.php
@@ -21,8 +21,8 @@ class AppGetSettingsController extends Controller
     {
         return [
             'isAppActive' => 1,            // Приложение активно
-            'minOrderAmount' => 7500,         // Минимальная сумма заказа
-            'isCashPaymentActive' => 0   // Разрешена ли оплата наличными
+            'minOrderAmount' => 5000,         // Минимальная сумма заказа
+            'isCashPaymentActive' => 1   // Разрешена ли оплата наличными
         ];
     }
 }
