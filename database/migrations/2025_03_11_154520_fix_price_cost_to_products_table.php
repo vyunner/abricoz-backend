420a01019a6e93f15b2063f0bf67e97b3d3fac66
Vyacheslav <vyunner@gmail.com>
2025-03-28 12:59:38 +0500

    M  database/migrations/2025_03_11_154520_fix_price_cost_to_products_table.php

--- database/migrations/2025_03_11_154520_fix_price_cost_to_products_table.php
+++ database/migrations/2025_03_11_154520_fix_price_cost_to_products_table.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void
     {
+        // 1. Обновим все NULL значения на 0
+        DB::table('products')->whereNull('price_cost')->update(['price_cost' => 0]);
+
+        // 2. Изменим структуру поля
         Schema::table('products', function (Blueprint $table) {
             $table->integer('price_cost')->default(0)->nullable(false)->change();
         });
