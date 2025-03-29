a0ee3ff836c82c7eb7553092acba03edf9df51b8
Vyacheslav <vyunner@gmail.com>
2025-03-29 20:13:53 +0500

    M  database/seeders/ProductBekzodSeeder.php

--- database/seeders/ProductBekzodSeeder.php
+++ database/seeders/ProductBekzodSeeder.php
@@ -11,7 +11,7 @@ class ProductBekzodSeeder extends Seeder
     public function run()
     {
         // Путь к файлу Excel
-        $filePath = base_path('database/seeders/xlsx/печенюшки.xlsx');
+        $filePath = base_path('database/seeders/xlsx/Томирис Happy baby 2.xlsx');
 
         // Загружаем файл Excel
         try {
