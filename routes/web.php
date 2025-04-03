16af70b32e3be6f2badf9bc6d6c75747f90f3699
Vyacheslav <vyunner@gmail.com>
2025-04-03 12:06:27 +0500

    M  routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Telegram\Bot\Laravel\Facades\Telegram;
 use Illuminate\Http\Request;
 use App\Telegram\Commands\DailyOrdersCommand;
+use App\Telegram\Commands\OrderByIdCommand; // 👈 добавь импорт
 
 Route::view('/UHoxHPD8bV1sCc1uIj8lWUmO', 'otp-verification')->name('welcome');
 Route::post('/send-otp', WebAdmin\SendCodeController::class)->middleware(['throttle:1,1'])->name('send.otp');
@@ -17,16 +18,27 @@ Route::get('/dashboard', function () {
 Route::post('/telegram/webhook', function (Request $request) {
     $update = Telegram::getWebhookUpdate();
 
-    // Обработка force reply
+    // Обработка force reply для /orders
     if (
         $update->isType('message') &&
         $update->getMessage()->getReplyToMessage() &&
         str_contains($update->getMessage()->getReplyToMessage()->getText(), 'Введите число текущего месяца')
     ) {
         (new DailyOrdersCommand())->processMessage($update);
-        return response()->json(['status' => 'processed']);
+        return response()->json(['status' => 'orders processed']);
     }
 
+    // Обработка force reply для /order
+    if (
+        $update->isType('message') &&
+        $update->getMessage()->getReplyToMessage() &&
+        str_contains($update->getMessage()->getReplyToMessage()->getText(), 'Введите ID заказа')
+    ) {
+        (new OrderByIdCommand())->processMessage($update);
+        return response()->json(['status' => 'order processed']);
+    }
+
+    // Все остальные команды
     Telegram::commandsHandler(true);
     return response()->json(['status' => 'ok']);
 });
