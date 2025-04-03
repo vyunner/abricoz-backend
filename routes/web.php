98dc9843f9d93ab317ab6defdc35f35a4a0b2d0c
Vyacheslav <vyunner@gmail.com>
2025-04-03 10:55:23 +0500

    M  routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\WebAdmin;
 use Illuminate\Support\Facades\Route;
 use Telegram\Bot\Laravel\Facades\Telegram;
 use Illuminate\Http\Request;
-use App\Telegram\Commands\DailyOrdersCommand;
 
 Route::view('/UHoxHPD8bV1sCc1uIj8lWUmO', 'otp-verification')->name('welcome');
 Route::post('/send-otp', WebAdmin\SendCodeController::class)->middleware(['throttle:1,1'])->name('send.otp');
@@ -14,22 +13,7 @@ Route::get('/dashboard', function () {
     return "You are logged in!";
 })->middleware('auth')->name('dashboard');
 
-
 Route::post('/telegram/webhook', function (Request $request) {
-    $update = Telegram::getWebhookUpdate();
-
-    // Обрабатываем force reply вручную
-    if (
-        $update->isType('message') &&
-        $update->getMessage()->getReplyToMessage() &&
-        str_contains($update->getMessage()->getReplyToMessage()->getText(), 'Введите число текущего месяца')
-    ) {
-        (new DailyOrdersCommand())->processMessage($update);
-        return response()->json(['status' => 'reply processed']);
-    }
-
-    // Обработка обычных команд
     Telegram::commandsHandler(true);
-
     return response()->json(['status' => 'ok']);
 });
