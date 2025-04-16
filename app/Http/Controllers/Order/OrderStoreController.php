68fc3fbe0204a09ed8f07166add9005dc37becb1
Vyacheslav <vyunner@gmail.com>
2025-04-16 11:52:37 +0500

    M  app/Http/Controllers/Order/OrderStoreController.php

--- app/Http/Controllers/Order/OrderStoreController.php
+++ app/Http/Controllers/Order/OrderStoreController.php
@@ -127,7 +127,7 @@ class OrderStoreController extends Controller
             $productsData[] = [
                 'product' => $product,
                 'quantity' => $productItem['product_quantity'],
-                'stock_quantity' => $productItem['stock_quantity'],
+                'stock_quantity' => $product->stock_quantity,
                 'price' => $product->price,
                 'discount' => $product->discount,
                 'price_with_discount' => $product->price_with_discount,
