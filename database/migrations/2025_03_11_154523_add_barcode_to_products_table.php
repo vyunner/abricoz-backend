d8795c0396b31db451af3c64a79002ee1dda25a7
Vyacheslav <vyunner@gmail.com>
2025-04-21 13:13:58 +0500

    M  database/migrations/2025_03_11_154523_add_barcode_to_products_table.php

--- database/migrations/2025_03_11_154523_add_barcode_to_products_table.php
+++ database/migrations/2025_03_11_154523_add_barcode_to_products_table.php
@@ -8,14 +8,13 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('barcode')->unique()->after('subcategory_id');
+            $table->string('barcode')->after('subcategory_id');
         });
     }
 
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropUnique(['barcode']);
             $table->dropColumn('barcode');
         });
     }
