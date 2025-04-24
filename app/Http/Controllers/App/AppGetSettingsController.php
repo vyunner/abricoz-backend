c166d8894ecc83747fe8aacbd29f845631203dc7
Vyacheslav <vyunner@gmail.com>
2025-04-24 19:10:28 +0500

    M  app/Http/Controllers/App/AppGetSettingsController.php

--- app/Http/Controllers/App/AppGetSettingsController.php
+++ app/Http/Controllers/App/AppGetSettingsController.php
@@ -20,7 +20,7 @@ class AppGetSettingsController extends Controller
     public function __invoke(Request $request)
     {
         return [
-            'isAppActive' => 1,            // Приложение активно
+            'isAppActive' => 0,            // Приложение активно
             'minOrderAmount' => 5000,         // Минимальная сумма заказа
             'isCashPaymentActive' => 1,   // Разрешена ли оплата наличными
             'isBankPaymentActive' => 0   // Разрешена ли оплата банком
