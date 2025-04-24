48785c3c749a94895cfe33e247904736597131e3
Vyacheslav <vyunner@gmail.com>
2025-04-24 16:54:09 +0500

    M  app/Http/Controllers/Pos/PosGetSubcategories.php

--- app/Http/Controllers/Pos/PosGetSubcategories.php
+++ app/Http/Controllers/Pos/PosGetSubcategories.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\Pos;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Subcategory;
+use App\Models\SubCategory;
 
 class PosGetSubcategories extends Controller
 {
     public function __invoke(Request $request)
     {
-        $subcategories = Subcategory::orderBy('name_ru')->get();
+        $subcategories = SubCategory::orderBy('name_ru')->get();
         return response()->json($subcategories);
     }
 }
