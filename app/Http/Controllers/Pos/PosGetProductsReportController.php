3c44260a419392f6866c9c5f2d3ac0b02ebb4a63
Vyacheslav <vyunner@gmail.com>
2025-04-25 15:12:33 +0500

    M  app/Http/Controllers/Pos/PosGetProductsReportController.php

--- app/Http/Controllers/Pos/PosGetProductsReportController.php
+++ app/Http/Controllers/Pos/PosGetProductsReportController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Pos;
 
 use App\Http\Controllers\Controller;
-use App\Models\Subcategory;
+use App\Models\SubCategory;
 use Illuminate\Support\Facades\DB;
 
 class PosGetProductsReportController extends Controller
 {
     public function __invoke()
     {
-        $subcategories = Subcategory::select('id', 'name_ru')
+        $subcategories = SubCategory::select('id', 'name_ru')
             ->with(['products' => function ($query) {
                 $query->select('id', 'subcategory_id', 'name_ru', 'weight', 'amount', 'stock_quantity')
                     ->where('is_active', 1);
