0744c4553bc1a9a95aa434480348de65c70c5a1a
Vyacheslav <vyunner@gmail.com>
2025-04-26 14:44:57 +0500

    M  app/Http/Controllers/Pos/PosUploadProductPhotoController.php

--- app/Http/Controllers/Pos/PosUploadProductPhotoController.php
+++ app/Http/Controllers/Pos/PosUploadProductPhotoController.php
@@ -15,6 +15,7 @@ class PosUploadProductPhotoController extends Controller
 {
     public function __invoke(Request $request)
     {
+        return config('services.openai.api_key');
         $request->validate([
             'photo' => 'required|file|mimes:jpg,jpeg,png,webp|max:5120',
         ]);
