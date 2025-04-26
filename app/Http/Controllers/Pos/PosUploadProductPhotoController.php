0ebdeea77f5cfb67f4e872f822f9f30cb848683f
Vyacheslav <vyunner@gmail.com>
2025-04-26 13:13:07 +0500

    M  app/Http/Controllers/Pos/PosUploadProductPhotoController.php

--- app/Http/Controllers/Pos/PosUploadProductPhotoController.php
+++ app/Http/Controllers/Pos/PosUploadProductPhotoController.php
@@ -27,7 +27,7 @@ class PosUploadProductPhotoController extends Controller
         $manager = new ImageManager(new Driver());
         $image = $manager->read($original)
             ->cover(500, 500)        // обрезает и вписывает в 500x500 сохраняя пропорции
-            ->toWebp(quality: 100);    // сохраняет в webp с качеством
+            ->toWebp(quality: 95);    // сохраняет в webp с качеством
 
         $filename = 'products/' . Str::uuid() . '.webp';
 
