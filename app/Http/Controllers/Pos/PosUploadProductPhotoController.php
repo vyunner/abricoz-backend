075473fce89c81678976877294421e4a765fa8a5
Vyacheslav <vyunner@gmail.com>
2025-04-26 12:51:55 +0500

    M  app/Http/Controllers/Pos/PosUploadProductPhotoController.php

--- app/Http/Controllers/Pos/PosUploadProductPhotoController.php
+++ app/Http/Controllers/Pos/PosUploadProductPhotoController.php
@@ -16,8 +16,6 @@ class PosUploadProductPhotoController extends Controller
     public function __invoke(Request $request)
     {
         Log::info('Файлы, пришедшие с фронта:', $request->allFiles());
-        dd($request->allFiles());
-
         $request->validate([
             'photo' => 'required|file|mimes:jpg,jpeg,png,webp|max:5120',
         ]);
