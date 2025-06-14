b5c9053a4a13ccdeca5d29e8efca99d0af0a3d8e
Vyacheslav <vyunner@gmail.com>
2025-06-14 22:56:46 +0500

    M  app/Services/TelegramService.php

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -3,15 +3,29 @@
 namespace App\Services;
 
 use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Exceptions\TelegramResponseException;
+use Illuminate\Support\Facades\Log;
 
 class TelegramService
 {
     public function sendMessage($chatId, $message, $parse_mode = 'HTML')
     {
-        return Telegram::bot('mybot')->sendMessage([
-            'chat_id' => $chatId,
-            'text' => $message,
-            'parse_mode' => $parse_mode,
-        ]);
+        try {
+            return Telegram::bot('mybot')->sendMessage([
+                'chat_id' => $chatId,
+                'text' => $message,
+                'parse_mode' => $parse_mode,
+            ]);
+        } catch (TelegramResponseException $e) {
+            if (str_contains($e->getMessage(), 'bot was blocked by the user')) {
+                Log::warning("Пользователь заблокировал Telegram-бота. chat_id: {$chatId}");
+                return null; // Молча уходим
+            }
+
+            throw $e; // Другие ошибки — пробрасываем
+        } catch (\Throwable $e) {
+            Log::error("Ошибка отправки сообщения в Telegram: " . $e->getMessage());
+            return null;
+        }
     }
 }
